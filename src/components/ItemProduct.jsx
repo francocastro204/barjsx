8f0dd2c6b2b786f937ccc7205d29694330541a7f
francocastro204 <francocastro204@gmail.com>
2025-09-23 01:13:44 -0300

    se actualiza SKU en ItemProduct. CreaTuLanding1+Castro

--- src/components/ItemProduct.jsx
+++ src/components/ItemProduct.jsx
@@ -15,7 +15,8 @@ const ItemProduct = ({ sku, title, price, image, description, categoria }) => {
                 <div className="w-2/3 p-4">
                     <h3 className="font-bold text-lg">{title}</h3>
                     <h4 className="text-sm font-bold">${price}</h4>
-                    <small className=" text-gray-500"><i>{sku}</i></small>
+                    <small className=" text-gray-500"><i>SKU: {sku}</i></small>
+                    <br />
                     <small className=" text-gray-900">{description}</small>
                     <br />
                     <small className="text-gray-500 text-xs"><i>{categoria}</i></small>

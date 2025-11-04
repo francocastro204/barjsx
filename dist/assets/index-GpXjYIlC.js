0f9e37a1d40da1380357547c65eb8e98b77bd456
francocastro204 <francocastro204@gmail.com>
2025-11-04 20:53:12 -0300

    feat: se modifico el .gitignore y otros

--- dist/assets/index-GpXjYIlC.js
+++
@@ -1,1 +0,0 @@
-import{a8 as a}from"./features-animation-BQeWi7b8.js";import"./index-B2SqOw1U.js";var i=a;export{i as default};
--- dist/assets/src-UW24ZMRV-CRgbzoPT.js
+++
@@ -1,1 +0,0 @@
-import{a8 as a}from"./features-animation-BQeWi7b8.js";import"./index-B2SqOw1U.js";var t=a;export{t as default};
